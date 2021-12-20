14cec2f309724d7531b62f601f8b8b6eabfdccc6
uyjulian <uyjulian@gmail.com>
2021-12-19 22:25:33 -0600

    Add an handler for unhandled exception

--- ncbind.cpp
+++ ncbind.cpp
@@ -26,6 +26,70 @@ BOOLEAN WINAPI DllMain(HINSTANCE hDllHandle, DWORD nReason, LPVOID lpReserved)
 	return TRUE;
 }
 
+static iTVPFunctionExporter * FunctionExporter = NULL;
+
+//---------------------------------------------------------------------------
+#include <iostream>
+#include <sstream>
+#include <exception>
+#include <stdexcept>
+#include <cstdlib>
+#ifndef __clang__
+#include <cxxabi.h>
+#endif
+
+static void std_terminate_handler()
+{
+	std::stringstream ss;
+	auto const ep = std::current_exception();
+	if (ep)
+	{
+		ss << "Unhandled exception from plugin";
+		if (DllHandle != NULL)
+		{
+			ss << " ";
+			{
+				CHAR* modnamebuf = (CHAR*)malloc(32768 * sizeof(CHAR));
+				if (modnamebuf)
+				{
+					DWORD retLen = GetModuleFileNameA(DllHandle, modnamebuf, 32768);
+					if (retLen)
+					{
+						ss << modnamebuf;
+					}
+					free(modnamebuf);
+				}
+			}
+		}
+		ss << ":";
+		try
+		{
+			ss << "Terminating with uncaught exception";
+#ifndef __clang__
+			int status;
+			auto const etype = abi::__cxa_demangle(abi::__cxa_current_exception_type()->name(), 0, 0, &status);
+			if (etype != NULL)
+			{
+				ss << " of type `" << etype << "`";
+			}
+#endif
+			std::rethrow_exception(ep);
+		}
+		catch(const std::exception& e)
+		{
+			ss << " with `what()` = \"" << e.what() << "\"";
+		}
+		catch(...) {}
+		std::cerr << ss.str();
+		if (FunctionExporter != NULL)
+		{
+			ttstr ssttstr(ss.str().c_str());
+			TVPThrowExceptionMessage(ssttstr.c_str());
+		}
+	}
+	std::abort();
+}
+
 //---------------------------------------------------------------------------
 static tjs_int GlobalRefCountAtInit = 0;
 
@@ -33,6 +97,8 @@ EXPORT(HRESULT) V2Link(iTVPFunctionExporter *exporter)
 {
 	// スタブの初期化(必ず記述する)
 	TVPInitImportStub(exporter);
+	FunctionExporter = exporter;
+	std::set_terminate(std_terminate_handler);
 
 	NCB_LOG_W("V2Link");
 
